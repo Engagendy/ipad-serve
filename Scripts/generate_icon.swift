0f75881c08859d24ff9bc2be7e9eb2a14607100e
Enagendy <engagendy@emagile.com>
2026-05-13 14:31:07 +0400

    Fix iPad app icon packaging

--- Scripts/generate_icon.swift
+++ Scripts/generate_icon.swift
@@ -1,22 +1,22 @@
 import AppKit
 
-let size = 1024
-let scale = CGFloat(size)
-let output = URL(fileURLWithPath: "iPadServe/Resources/Assets.xcassets/AppIcon.appiconset/icon-1024.png")
+let baseSize = 1024
+let scale = CGFloat(baseSize)
+let outputDirectory = URL(fileURLWithPath: "iPadServe/Resources/Assets.xcassets/AppIcon.appiconset")
 
-let image = NSImage(size: NSSize(width: size, height: size))
+let image = NSImage(size: NSSize(width: baseSize, height: baseSize))
+
+func roundedRect(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat, _ radius: CGFloat) -> NSBezierPath {
+    NSBezierPath(roundedRect: NSRect(x: x, y: y, width: width, height: height), xRadius: radius, yRadius: radius)
+}
+
 image.lockFocus()
-
 let rect = NSRect(x: 0, y: 0, width: scale, height: scale)
 let background = NSGradient(colors: [
     NSColor(calibratedRed: 0.04, green: 0.18, blue: 0.29, alpha: 1),
     NSColor(calibratedRed: 0.02, green: 0.47, blue: 0.58, alpha: 1)
 ])!
 background.draw(in: rect, angle: 135)
-
-func roundedRect(_ x: CGFloat, _ y: CGFloat, _ w: CGFloat, _ h: CGFloat, _ r: CGFloat) -> NSBezierPath {
-    NSBezierPath(roundedRect: NSRect(x: x, y: y, width: w, height: h), xRadius: r, yRadius: r)
-}
 
 NSColor(calibratedWhite: 1, alpha: 0.10).setFill()
 roundedRect(116, 124, 792, 776, 92).fill()
@@ -67,11 +67,57 @@ roundedRect(706, 172, 72, 26, 13).fill()
 
 image.unlockFocus()
 
-guard let tiff = image.tiffRepresentation,
-      let bitmap = NSBitmapImageRep(data: tiff),
-      let png = bitmap.representation(using: .png, properties: [:]) else {
-    fatalError("Could not render app icon")
+struct IconSlot {
+    let filename: String
+    let pixels: Int
 }
 
-try FileManager.default.createDirectory(at: output.deletingLastPathComponent(), withIntermediateDirectories: true)
-try png.write(to: output)
+let icons = [
+    IconSlot(filename: "icon-ipad-20.png", pixels: 20),
+    IconSlot(filename: "icon-ipad-20@2x.png", pixels: 40),
+    IconSlot(filename: "icon-ipad-29.png", pixels: 29),
+    IconSlot(filename: "icon-ipad-29@2x.png", pixels: 58),
+    IconSlot(filename: "icon-ipad-40.png", pixels: 40),
+    IconSlot(filename: "icon-ipad-40@2x.png", pixels: 80),
+    IconSlot(filename: "icon-ipad-76.png", pixels: 76),
+    IconSlot(filename: "icon-ipad-76@2x.png", pixels: 152),
+    IconSlot(filename: "icon-ipad-83.5@2x.png", pixels: 167),
+    IconSlot(filename: "icon-1024.png", pixels: 1024)
+]
+
+func pngData(from source: NSImage, pixels: Int) -> Data {
+    guard let bitmap = NSBitmapImageRep(
+        bitmapDataPlanes: nil,
+        pixelsWide: pixels,
+        pixelsHigh: pixels,
+        bitsPerSample: 8,
+        samplesPerPixel: 4,
+        hasAlpha: true,
+        isPlanar: false,
+        colorSpaceName: .deviceRGB,
+        bytesPerRow: 0,
+        bitsPerPixel: 0
+    ) else {
+        fatalError("Could not create \(pixels)x\(pixels) app icon bitmap")
+    }
+
+    let targetSize = NSSize(width: pixels, height: pixels)
+    bitmap.size = targetSize
+
+    NSGraphicsContext.saveGraphicsState()
+    NSGraphicsContext.current = NSGraphicsContext(bitmapImageRep: bitmap)
+    NSGraphicsContext.current?.imageInterpolation = .high
+    source.draw(in: NSRect(origin: .zero, size: targetSize), from: NSRect(origin: .zero, size: source.size), operation: .copy, fraction: 1)
+    NSGraphicsContext.restoreGraphicsState()
+
+    guard let png = bitmap.representation(using: .png, properties: [:]) else {
+        fatalError("Could not render \(pixels)x\(pixels) app icon")
+    }
+    return png
+}
+
+try FileManager.default.createDirectory(at: outputDirectory, withIntermediateDirectories: true)
+
+for icon in icons {
+    try pngData(from: image, pixels: icon.pixels).write(to: outputDirectory.appendingPathComponent(icon.filename))
+}
